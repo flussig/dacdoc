ca893ed15e0839bbcb3e7569af602db7e799738f
Serdar Kurbanov <serdar.kurbanov@hotmail.com>
2019-09-02 22:13:01 -0400

    more proper test of git blame util

--- dacdoc-git/src/main/java/com/github/flussig/dacdoc/GitBlameLineDetails.java
+++ dacdoc-git/src/main/java/com/github/flussig/dacdoc/GitBlameLineDetails.java
@@ -8,6 +8,12 @@ import java.util.List;
  * Info associated with one line of file under git blame
  * */
 public class GitBlameLineDetails {
+    public static final String LINE_CONTENT_INDICATOR = "\t";
+    public static final String SEPARATOR = " ";
+    private static final String AUTHOR_LINE_INDICATOR = "author";
+    private static final String AUTHOR_EMAIL_LINE_INDICATOR = "author-mail";
+    private static final String AUTHOR_TIME_INDICATOR = "author-time";
+
     // gets line details from strings that form the block for 1 line of blamed file
     // assumption is that input is from 'git blame --line-porcelain' command
     // porcelain format description: https://git-scm.com/docs/git-blame
@@ -17,11 +23,11 @@ public class GitBlameLineDetails {
         for(int i = 0; i < strings.size(); i++) {
             String currentLine = strings.get(i);
 
-            if(Strings.isNullOrEmpty(currentLine) && currentLine.startsWith("\t")) {
+            if(Strings.isNullOrEmpty(currentLine) && currentLine.startsWith(LINE_CONTENT_INDICATOR)) {
                 continue;
             }
 
-            String[] lineParts = currentLine.split(" ");
+            String[] lineParts = currentLine.split(SEPARATOR);
 
             if(lineParts.length < 2) {
                 continue;
@@ -34,22 +40,35 @@ public class GitBlameLineDetails {
             }
 
             // extract user that made last modification
-            if(lineParts[0].equals("author")) {
-                result.setUser(lineParts[1]);
+            if(lineParts[0].equals(AUTHOR_LINE_INDICATOR)) {
+                result.setUser(joinParts(lineParts, 1, lineParts.length - 1));
             }
 
             // extract email of the user that made las modification
-            if(lineParts[0].equals("author-mail")) {
-                result.setUserEmail(lineParts[1]);
+            if(lineParts[0].equals(AUTHOR_EMAIL_LINE_INDICATOR)) {
+                result.setUserEmail(joinParts(lineParts, 1, lineParts.length - 1));
             }
 
             // extract author epoch second
-            if(lineParts[0].equals("author-time")) {
+            if(lineParts[0].equals(AUTHOR_TIME_INDICATOR)) {
                 result.setEpochSecond(Long.valueOf(lineParts[1]));
             }
         }
 
         return result;
+    }
+
+    private static String joinParts(String[] parts, int start, int end) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = start; i <= end; i++) {
+            sb.append(parts[i]);
+            if(i != end) {
+                sb.append(SEPARATOR);
+            }
+        }
+
+        return sb.toString();
     }
 
     private GitBlameLineDetails() {}
--- dacdoc-git/src/test/java/com/github/flussig/dacdoc/GitUtilTest.java
+++ dacdoc-git/src/test/java/com/github/flussig/dacdoc/GitUtilTest.java
@@ -1,12 +1,14 @@
 package com.github.flussig.dacdoc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit check for simple GitUtil.
@@ -24,5 +26,20 @@ public class GitUtilTest
         List<GitBlameLineDetails> gitBlameLineDetails = GitUtil.getBlameDetails(readmeFile);
 
         assertEquals(3, gitBlameLineDetails.size());
+
+        // not sure if asserting concrete values for git blame output is a good idea here (may change with later commits), so non-null checks are made
+        assertTrue(gitBlameLineDetails.stream().noneMatch(line -> line.getCommitId() == null));
+        assertTrue(gitBlameLineDetails.stream().noneMatch(line -> line.getUser() == null));
+        assertTrue(gitBlameLineDetails.stream().noneMatch(line -> line.getUserEmail() == null));
+        assertTrue(gitBlameLineDetails.stream().noneMatch(line -> line.getEpochSecond() == null));
+        assertTrue(gitBlameLineDetails.stream().noneMatch(line -> line.getLineNumber() == null));
+
+        // check git line numbers
+        Set<Integer> gitLineNumbers = gitBlameLineDetails.stream()
+                .map(GitBlameLineDetails::getLineNumber)
+                .collect(Collectors.toSet());
+        List<Integer> expectedLineNumbers = Arrays.asList(1,2,3);
+        assertEquals(3, gitLineNumbers.size());
+        assertTrue(gitLineNumbers.containsAll(expectedLineNumbers));
     }
 }
